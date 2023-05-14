5e3c4a78d09d3b38f407c2c0203c5e2bc40b2fba
sheepla <62412884+sheepla@users.noreply.github.com>
2023-05-14 19:51:34 +0900

    Fix CLI args check

--- cli.ts
+++ cli.ts
@@ -5,18 +5,8 @@ const { options, args } = await new Command()
   .version("v0.0.1")
   .description("A simple command to expand Emmet expression")
   .option("-s, --stylesheet", "Stylesheet mode")
-  .arguments("<expr:string> [text:string]")
+  .arguments("<expr:string>")
   .parse(Deno.args);
-
-if (args.length < 1) {
-  console.error("Must require arguments.");
-  Deno.exit(1);
-}
-
-if (1 < args.length) {
-  console.error("Too many arguments.");
-  Deno.exit(1);
-}
 
 const result: string = expand(args[0], {
   type: options.stylesheet ? "stylesheet" : "markup",
